c6b0cbc97389c3478ea57e4592143cc6f08b28e1
MarcelJurtz <jurtzmarcel@gmail.com>
2017-08-12 12:17:17 +0200

    Add menu scene

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LevelLoader.LoadMainScene();
+        }
+    }
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+	private static void LoadLevel(Levels level)
+    {
+        if(level.Equals(Levels.MAIN))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+        else if(level.Equals(Levels.MENU))
+        {
+            SceneManager.LoadScene("MenuScene");   
+        }
+    }
+
+    public static void LoadMenuScene()
+    {
+        LoadLevel(Levels.MENU);
+    }
+
+    public static void LoadMainScene()
+    {
+        LoadLevel(Levels.MAIN);
+    }
+}
+
+public enum Levels
+{
+    MENU, MAIN
+}
