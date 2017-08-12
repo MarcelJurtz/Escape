917ef756a6c879c153ce58036d7c046ce24e1b9b
MarcelJurtz <jurtzmarcel@gmail.com>
2017-08-12 12:29:10 +0200

    Add highscore functionality

---
+++ Assets/Scripts/HighscoreManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HighscoreManager : MonoBehaviour {
+
+    private const string prefsKey = "Highscore";
+    private const int defaultHighscore = 0;
+
+	public static void setHighscore(int highscore)
+    {
+        if(highscore > getHighscore())
+        {
+            PlayerPrefs.SetInt(prefsKey, highscore);
+        }     
+    }
+
+    public static int getHighscore()
+    {  
+        return (int)PlayerPrefs.GetInt(prefsKey, defaultHighscore);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -83,6 +83,7 @@ public class PlayerController : MonoBehaviour {
     {
         isLiving = false;
         instructionText.text = "GAME OVER";
+        HighscoreManager.setHighscore(score);
     }
 
     public MovementAllowed getAllowedDirections()
