3211a2125d81f8ca16d94d68b0b20a01d7c71aa6
MarcelJurtz <jurtzmarcel@gmail.com>
2017-08-17 13:29:03 +0200

    Replace start button with touch or tap

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour {
+
+    GestureRecognizer swipeControls;
+
+	void Start () {
+        swipeControls = GetComponent<GestureRecognizer>();
+	}
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) || swipeControls.Tap)
+        {
+            GetComponent<LevelLoader>().LoadMainScene();
+        }
+
+    }
+}
--- Assets/Scripts/GestureRecognizer.cs
+++ Assets/Scripts/GestureRecognizer.cs
@@ -21,6 +21,7 @@ public class GestureRecognizer : MonoBehaviour {
     public bool SwipeRight { get { return swipeRight; } }
     public bool SwipeUp { get { return swipeUp; } }
     public bool SwipeDown { get { return swipeDown; } }
+    public bool Tap { get { return tap; } }
 
     private void Update()
     {
